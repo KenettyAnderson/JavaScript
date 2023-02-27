3b16db6335e3557b10f2b2394ec33bddee24781b
Kenetty Anderson <kenettyanderson@hotmail.com>
2023-02-26 22:32:50 -0300

    d007 ajuste

--- desafios/d007/funcoes.js
+++ desafios/d007/funcoes.js
@@ -1,14 +1,16 @@
 // function cotacao() {
 //     var cotacao = parseFloat(prompt('Antes de mais nada. Quanto está a cotação do dólar agora?'))
+//     return cotacao
+// }
     function converter() {
         var res = window.document.querySelector('div#res')
-        cotacao = 5.17
+        var cotacao = 5.17
 
-        var dinheirobr = parseFloat(prompt('Quantos R$ você tem na carteira?'))
+        var dinheiroBR = parseFloat(prompt('Quantos R$ você tem na carteira?'))
 
-        var dinheirous = parseFloat(dinheirobr * cotacao)
+        var dinheiroUS = parseFloat(dinheiroBR * cotacao)
 
     
-        res.innerHTML = (`Você tem ${dinheirous} dólares.`)
+        res.innerHTML = (`Você tem ${dinheiroUS} dólares.`)
     }
-//}
+
