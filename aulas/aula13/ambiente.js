da8ccb6b77a3ca905d131aa26caef9b5c8cecce7
Kenetty Anderson <kenettyanderson@hotmail.com>
2023-03-05 19:01:12 -0300

    rename/aula13/aula14

---
+++ aulas/aula13/ambiente.js
@@ -0,0 +1,23 @@
+// FORMA "BRUTA" DE ESCREVER PASSO 1 À 6
+// console.log('Passo 1')
+// console.log('Passo 2')
+// console.log('Passo 3')
+// console.log('Passo 4')
+// console.log('Passo 5')
+// console.log('Passo 6')
+
+
+c = 1
+
+// ESCREVENDO PASSO 1 À 6 COM ESTRUTURA DE REPETIÇÃO COM TESTE LOGICO NO INICIO
+// while ( c <= 6) {
+//     console.log(`Passo ${c}`)
+//     c++
+// }
+
+
+// ESCREVENDO PASSO 1 À 6 COM ESTRUTURA DE REPETIÇÃO COM TESTE LOGICO NO FIM
+do {
+ console.log(`Passo ${c}`)
+ c++
+} while ( c <= 6)
---
+++ aulas/aula14/ambiente.js
@@ -0,0 +1,7 @@
+// ESTRUTURA DE REPETIÇÃO FOR
+
+console.log('Vai começar...')
+for (var c= 1; c <= 6; c++) {
+    console.log(c)
+}
+console.log('FIM')
