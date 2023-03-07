702aee5e38456449a8ca7ee7133a57455496a20a
Kenetty Anderson <kenettyanderson@hotmail.com>
2023-03-07 13:56:00 -0300

    fix function contar()

--- aulas/aula14ex/ex16/contar.js
+++ aulas/aula14ex/ex16/contar.js
@@ -2,28 +2,33 @@ function contar() {
     let ninicio = window.document.getElementById('ninicio')
     let nfim = window.document.getElementById('nfim')
     let npasso = window.document.getElementById('npasso')
+    let res = window.document.getElementById('res')
     if (ninicio.value.length == 0 || nfim.value.length == 0) {
         window.alert('O número de inicio e fim precisam ser informados.')
+        res.innerHTML = 'Preparando a contagem...'
     }
     
-    if(npasso.value.length == 0) {
-            window.alert('Considerando passo 1')
+    if(npasso.value.length == 0 || npasso <= 0) {
+            window.alert('Passo invalido, considerando PASSO 1.')
             npasso = 1
     }
 
-    let res = window.document.getElementById('res')
+    res.innerHTML = 'Contando: <br>'
 
-    // do {
-    //     res.innerHTML =
-    //     ninicio = ninicio + npasso
-    // } while (ninicio <= nfim)
+    let i = Number(ninicio.value)
+    let f = Number(nfim.value)
+    let p = Number(npasso.value)
 
-    // while (ninicio <= nfim) {
-    //     res.innerHTML = (`${ninicio}`)
-    //     ninicio = ninicio + npasso
-    // }
+    if (i < f) {
+        while (i <= f) {
+            res.innerHTML += (`${i} \u{1F449}`)
+            i += p
+        }
 
-    // for (c = ninicio; ninicio <= nfim; c = npasso ) {
-    //     res.innerHTML = ninicio
-    //}    
+        } else if (i > f) {
+            for (c = i; c >= f; c -= p) {
+            res.innerHTML += (`${c} \u{1F449}`)
+            }
+        }   
+        res.innerHTML += (` \u{1F3C1}`)
 }
