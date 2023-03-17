9292455b1171c5a1990ff9f0d6b84d4e9aa345b1
Kenetty Anderson <kenettyanderson@hotmail.com>
2023-03-17 15:29:43 -0300

    Update tabuada.js

--- aulas/aula14ex/ex17/tabuada.js
+++ aulas/aula14ex/ex17/tabuada.js
@@ -1,6 +1,6 @@
 function tabuada() {
     let num = window.document.getElementById('numero')
-    let res = window.document.getElementById('res')
+    let tab = window.document.getElementById('tabuada')
 
     if (num.value.length == 0) {
         window.alert ('Um número precisa ser informado')
@@ -8,13 +8,13 @@ function tabuada() {
         var n = Number(num.value)
     }
 
-    res.innerHTML = ''
-
     c = 1
 
+    tab.innerHTML = ''
     while (c <= 10){
-        res.text = `${n} x ${c} = ${n*c}`
-        option.appendChild (res)
+        var item = document.createElement('option')
+        item.text = `${n} x ${c} = ${n*c}`
+        tab.appendChild(item)
         c++
     }
     
